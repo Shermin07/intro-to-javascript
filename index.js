6236107e76336932e1e61f91b6d36d1463726983
Shermin Sultana <rima64549@gmail.com>
2023-07-29 02:55:28 +0600

    add

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ var shopKeeperWillReturn = givenMoney - totalCost;
 console.log(shopKeeperWillReturn);
 
 // practice problem:2//
+var mathemetics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks =  mathemetics + biology + chemistry +  physics +  bangla;
+console.log(totalMarks);
+var subjectNumber = 5;
+var averageNumber = totalMarks / subjectNumber;
+console.log(averageNumber);
+
+
+// practice problem:3//
+var firstVariable = "I am going to be"
+var secondVariable = "an awesome web developer"
+
+var output = firstVariable +" " +  secondVariable;
+console.log(output);
+
+// practice problem:4//
+var givenNumber = 119;
+var dividisionNumber = 5;
+var remainder = givenNumber % dividisionNumber;
+console.log(remainder);
 
