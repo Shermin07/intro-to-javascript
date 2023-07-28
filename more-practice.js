a6b889364cd5dd18fe2e97efd5bf212bb06cd95b
Shermin Sultana <rima64549@gmail.com>
2023-07-29 04:14:44 +0600

    add

---
+++ more-practice.js
@@ -0,0 +1,40 @@
+//check variable data type//
+var name = "shermin sultana"
+var age = 29;
+var isJobLess = true;
+console.log(typeof (age));
+console.log(typeof (isJobLess));
+console.log(typeof (name));
+
+// update variables value
+var onionPrice = 30;
+onionPrice = 35;
+
+// add some values with previous value
+var onionPrice = 50;
+ onionPrice = onionPrice + 20;
+ console.log(onionPrice);
+ onionPrice += 20;
+
+ // oparations
+ var price1 = 20;
+ var price2 = 20;
+
+ var sum = price1 + price2;
+ console.log(sum);
+
+ var subtraction = price1 -price2;
+ console.log(subtraction);
+
+ var multiplication = price1 * price2;
+ console.log(multiplication);
+
+ var division = price1 / price2;
+ console.log(division);
+
+ var remainder = price1 % price2;
+ console.log("remainder will be: " + remainder );
+
+
+
+
