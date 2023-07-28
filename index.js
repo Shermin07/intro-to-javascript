fd52540c53a809bea88b668332ec5d4fdb800f79
Shermin Sultana <rima64549@gmail.com>
2023-07-29 02:39:38 +0600

    add

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(6+6);
+// practice problem:1//
+var givenMoney = 1000;
+var totalCost = 700;
+var shopKeeperWillReturn = givenMoney - totalCost;
+console.log(shopKeeperWillReturn);
+
+// practice problem:2//
+
