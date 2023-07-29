689a92741e214fe781b2c38aa71c46e4ef4900d8
Shermin Sultana <rima64549@gmail.com>
2023-07-30 05:41:48 +0600

    add

---
+++ array1-practice.js
@@ -0,0 +1,118 @@
+// problem 1:
+
+var fruits = ["Apple", "Banana", "Orange"];
+
+// index number
+  var indexNumber = fruits[2];
+  console.log(indexNumber);
+
+// banana position
+var bananaIndex = fruits.indexOf("Banana");
+console.log(bananaIndex);
+
+// banana replace with mango
+fruits[1] = "Mango";
+console.log(fruits);
+
+// remove
+fruits.pop("Orange")
+console.log(fruits);
+
+// add 
+fruits.push("Watermelon");
+console.log(fruits);
+
+
+// problem 2:
+
+var myScores = 85;
+
+if(myScores >= 80 ) {
+    console.log("my grade will be: A");
+}
+else if(myScores >= 60 ) {
+    console.log("my grade will be: B");
+}
+else if(myScores >= 50 ) {
+    console.log("my grade will be: C");
+}
+else if(myScores >= 40 ) {
+    console.log("my grade will be: D");
+}
+else  {
+    console.log("my grade will be: F");
+}
+
+
+var tomScores = 66;
+
+if(tomScores >= 80   ) {
+    console.log("my grade will be: A");
+}
+else if(tomScores >= 60 ) {
+    console.log("my grade will be: B");
+}
+else if(tomScores >= 50 ) {
+    console.log("my grade will be: C");
+}
+else if(tomScores >= 40 ) {
+    console.log("my grade will be: D");
+}
+else  {
+    console.log("my grade will be: F");
+}
+
+var jeneScores = 95;
+
+if(jeneScores >= 80   ) {
+    console.log("my grade will be: A");
+}
+else if(jeneScores >= 60 ) {
+    console.log("my grade will be: B");
+}
+else if(jeneScores >= 50 ) {
+    console.log("my grade will be: C");
+}
+else if(jeneScores >= 40 ) {
+    console.log("my grade will be: D");
+}
+else  {
+    console.log("my grade will be: F");
+}
+
+var peterScores = 56;
+if( peterScores >= 80   ) {
+    console.log("my grade will be: A");
+}
+else if( peterScores >= 60 ) {
+    console.log("my grade will be: B");
+}
+else if( peterScores >= 50 ) {
+    console.log("my grade will be: C");
+}
+else if( peterScores>= 40 ) {
+    console.log("my grade will be: D");
+}
+else  {
+    console.log("my grade will be: F");
+}
+
+var johnScores = 40;
+if( johnScores>= 80   ) {
+    console.log("my grade will be: A");
+}
+else if( johnScores >= 60 ) {
+    console.log("my grade will be: B");
+}
+else if( johnScores >= 50 ) {
+    console.log("my grade will be: C");
+}
+else if( johnScores >= 40 ) {
+    console.log("my grade will be: D");
+}
+else  {
+    console.log("my grade will be: F");
+}
+
+
+
