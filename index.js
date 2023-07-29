953badd399f3b194af661fac6c68f967c21f23e6
Shermin Sultana <rima64549@gmail.com>
2023-07-29 15:40:36 +0600

    add

--- index.js
+++ index.js
@@ -35,4 +35,68 @@ var remainder = givenNumber % dividisionNumber;
 console.log(remainder);
 
 
+// more practice:
+// 1:
+var tomato = 40;
+var carrot = 30;
+var onion = 20;
+
+totalCost = tomato + onion + carrot;
+console.log(totalCost);
+
+var givenMoney = 500;
+var returnMoney = givenMoney - totalCost;
+console.log(returnMoney);
+
+//2.
+var money = 1000;
+var students = 20;
+
+var eachStudent = money / students;
+console.log(eachStudent);
+
+//3.
+var baby = 30;
+var candyquantity = 10;
+
+var totalneeded = baby * candyquantity;
+console.log("total need :" + totalneeded);
+
+//4.
+var math = 60;
+var english = 70;
+var bangla = 80;
+
+var totalNumber = math + english + bangla;
+console.log(totalNumber);
+
+var averageNumber = totalNumber / 3;
+console.log(averageNumber);
+
+//5.
+var marks = "90"
+var mark = parseInt(marks);
+console.log(mark);
+
+//6.
+var number = 60.6789;
+var number = parseFloat(60.6789);
+console.log(number);
+
+// 7.
+var firstLine = "I want to go home"
+var secondLine = "coz i miss my family"
+var result = firstLine + " " + secondLine;
+console.log(result);
+
+//8.
+var price = 5000;
+price = 4000;
+
+var newPrice = price + 2000;
+console.log(newPrice);
+
+
+
+
 
