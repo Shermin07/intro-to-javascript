fbb970296fdcddffb60db40f9f9591f6b7082c2a
Shermin Sultana <rima64549@gmail.com>
2023-08-04 17:05:14 +0600

    add

---
+++ funtask.js
@@ -0,0 +1,23 @@
+// 1:
+function takaToDollar(taka){
+    const dollar = taka * 0.0092;
+    return dollar;
+}
+const result =  takaToDollar(2000);
+console.log(result);
+
+//2:
+
+function barbieKen(){
+        const line = "phero"
+       
+     if(line.length % 2 == 0){
+            console.log("Hi ken")
+        }
+        else{
+            console.log("Hi barbie")
+        }
+       
+
+}
+barbieKen("phero")
