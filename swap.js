7bcd6ed2cbfe28c743824c26e8359a810ff37410
Shermin Sultana <rima64549@gmail.com>
2023-08-05 16:36:33 +0600

    add

---
+++ swap.js
@@ -0,0 +1,13 @@
+// temp:
+let first = 10;
+let second = 20;
+
+ //let temp = first;
+//first = second;
+//second = temp;
+//console.log(first, second);
+
+// destructuring:
+
+[first, second] = [second, first];
+console.log(first, second);
