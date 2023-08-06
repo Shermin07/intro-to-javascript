9d5386181366f2bd05ed6082a6c27db7bc0fd996
Shermin Sultana <rima64549@gmail.com>
2023-08-06 15:25:40 +0600

    add

--- given-function-practice.js
+++ given-function-practice.js
@@ -18,3 +18,38 @@ function centimeterToMeter(centimeter){
 const centimeter = 200;
 const output = centimeterToMeter(centimeter);
 console.log(output);
+
+
+// paperRequirements:
+function paperRequirements(book1Quantity,book2Quantity,book3Quantity ){
+const book1Page = 100;
+const book2Page = 200;
+const book3Page = 300;
+
+const book1TotalPage = book1Page * book1Quantity;
+const book2TotalPage = book2Page * book2Quantity;
+const book3TotalPage = book3Page * book3Quantity;
+
+const totalPage = book1TotalPage + book2TotalPage + book3TotalPage;
+return totalPage;
+
+
+}
+const total = paperRequirements(5, 3, 10);
+console.log(total);
+
+// bestFriend......largest name ber korte hobe
+
+const namesN = ["alia", "shumi", "rimu", "shermin", "dalia", "noorjaha"];
+function bestFriend(names){
+      let largest = names[0];
+    for(let i = 0; i < names.length; i++){
+        const name = names[i];
+     if(name.length > largest.length){
+        largest = name;
+     }
+    }
+    return largest;
+}
+const largestName = bestFriend(namesN);
+console.log(largestName);
