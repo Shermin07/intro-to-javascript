d2966d8b02b6dd529a9e96792b846f21b7f27b95
Shermin Sultana <rima64549@gmail.com>
2023-08-06 14:46:44 +0600

    add

---
+++ given-function-practice.js
@@ -0,0 +1,20 @@
+// feetToInch:
+
+function feetToInch(feet){
+    inches = feet * 12;
+    return inches;
+
+}
+const feet = 12;
+const result = feetToInch(feet);
+console.log(result);
+
+// centimeterToMeter:
+
+function centimeterToMeter(centimeter){
+          meter = centimeter / 100;
+          return meter;
+}
+const centimeter = 200;
+const output = centimeterToMeter(centimeter);
+console.log(output);
