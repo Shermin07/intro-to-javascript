ce6fbd4d4915ae95a322fb3ef3c5cb0b2651b1ed
Shermin Sultana <rima64549@gmail.com>
2023-08-07 20:33:45 +0600

    add

--- js-problem5.js
+++ js-problem5.js
@@ -55,3 +55,58 @@ else{
 
 const result3 = mindGame(30);
 console.log(result3);
+
+// problem4:::
+
+function isGiven(number){
+    if(typeof number !== "number"){
+        return "please return a number"
+    }
+    else{
+        const differ = number-7;
+        if(differ < 7){
+            return differ;
+            }
+            else{
+                return number*2;
+            }
+    }
+
+}
+const result4 = isGiven(6);
+console.log(result4);
+const result41 = isGiven(-15);
+console.log(result41);
+const result42 = isGiven(15);
+console.log(result42);
+
+
+// problem5:::::
+
+const arr = [10, -11, 13, 14, 34, -23, -45, -55, 90, -100];
+
+function findingBadData(x){
+    if(!Array.isArray ){
+        return "please provide me an array"
+    }
+    else{ 
+        let badData = [];
+        for(let i = 0; i< x.length; i++){
+        if(typeof x[i] !== "number"){
+            return "please provide a positive number"
+        }
+        else{
+            if(x[i] < 0){
+                badData.push(x[i]);
+            }
+        }
+        }
+        return badData;
+    }
+    
+   
+
+}
+
+const result5 = findingBadData(arr);
+console.log(result5);
